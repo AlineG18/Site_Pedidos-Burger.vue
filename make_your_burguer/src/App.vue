0a2ba45f3b225986599d2175dcd71d40b7d39314
AlineG18 <103582602+AlineG18@users.noreply.github.com>
2024-07-02 11:29:34 -0300

    Aula5_Finalizando Rodapé

--- make_your_burguer/src/App.vue
+++ make_your_burguer/src/App.vue
@@ -32,4 +32,14 @@ export default {
   margin: 0px;
   box-sizing: border-box;
 }
+.main-container{
+  margin: 50px;
+  min-height: 250px;
+}
+h1{
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
--- make_your_burguer/src/components/Footer.vue
+++ make_your_burguer/src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-    <p>footer</p>
+    <footer id="footer">
+        <p>Make Your Burguer &copy; 2024</p>
+    </footer>
 </template>
 
 <script>
@@ -7,3 +9,16 @@ export default {
  name: "Footer"
 }
 </script>
+
+<style scoped>
+#footer{
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+}
+</style>
